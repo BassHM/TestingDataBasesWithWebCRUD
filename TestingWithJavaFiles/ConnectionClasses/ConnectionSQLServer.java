fbc81406a20ce4d41055c664110249a9e7981f30
Laremo <memoo.lara12306@hotmail.com>
2023-05-18 09:38:22 -0600

    Ya hay conector para el SQLServer, requiere la libreria mssql-jdbc-10.2.1.jre8 (o cualquier version mas reciente)

---
+++ TestingWithJavaFiles/ConnectionClasses/ConnectionSQLServer.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class Connector {
+    //Connection string = jdbc:sqlserver://NOMBREDELSERVER\\NOMBREDELServicio.database.windows.net:puerto(1433 por defecto);
+    private static final String URL = "jdbc:sqlserver://DESKTOP-U1NQ44F\\TESTDB.database.widows.net:1433;"
+            + "database = L_III_Gestion_Alumnos;" //nombre de la database
+            + "user = sa;" //nombre del usuario
+            + "password = @ThisIsThePassword1;" //contrase;a
+            + "encrypt = true;" 
+            + "trustServerCertificate=true;"
+            + "loginTimeout=30;";
+
+    
+    public static Connection getConnection() throws SQLException{
+        
+        try
+        (Connection con = DriverManager.getConnection(URL)){
+            
+        }
+        catch(SQLException ex ){
+            ex.printStackTrace();
+        }
+        
+        return DriverManager.getConnection(URL);
+    }
+    
+    public static void close(ResultSet rs) throws SQLException{
+        rs.close();
+    }
+    
+    public static void close(PreparedStatement stm) throws SQLException{
+        stm.close();
+    }
+    
+    public static void close(Connection con) throws SQLException {
+        con.close();
+    }
+}
